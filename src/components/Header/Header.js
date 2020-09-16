a0215b41e9e3300081d0e4b70913ad0ae7abe192
Orr-J-Sun <orr.sun10@gmail.com>
2020-09-16 15:33:14 +0300

    basic header structure

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <Container>
+        <HeaderWrapper>
+          <Logo />
+          <HeaderTitle />
+          <HeaderSubtitle />
+        </HeaderWrapper>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
