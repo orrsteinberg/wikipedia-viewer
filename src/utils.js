120dfb592d5ed0b51a1790eae5d4fb3677f48595
Orr Steinberg <orr.sun10@gmail.com>
2020-10-28 19:00:08 +0200

    comments

--- src/utils.js
+++ src/utils.js
@@ -2,12 +2,12 @@
 export const isEmpty = (obj) => Object.keys(obj).length === 0;
 
 // Parse and merge new entries with current ones to avoid duplicates
-export const mergeEntries = (newEntries, currentEntries = {}) => {
+export const mergeEntries = (newEntries, currentEntries) => {
   return newEntries.reduce((obj, entry) => {
     if (!obj[`_${entry.pageid}`]) {
       return {
         ...obj,
-        // Prefix to bypass auto-sorting of objects so that new entries are appended to the end
+        // Prefix _ to bypass auto-sorting of objects so that new entries are appended to the end
         [`_${entry.pageid}`]: entry,
       };
     }
