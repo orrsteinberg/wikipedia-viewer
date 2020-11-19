177b9620c9cdf0c028f0e83a4d748787399e5fd9
Orr Steinberg <orr.sun10@gmail.com>
2020-11-19 23:12:08 +0200

    update github icon url

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,17 @@
 import React from "react";
+import { GITHUB_URL } from "../../constants";
 import { FooterWrapper, GithubIcon } from "./Footer.elements";
 
 const Footer = () => {
   return (
     <FooterWrapper>
       <a
-        href="https://www.github.com/orrsteinberg"
+        href={GITHUB_URL}
         target="_blank"
         rel="noopener noreferrer"
+        aria-label="Github Repo Link"
       >
-        <GithubIcon />
+        <GithubIcon aria-hidden="true" focusable="false" />
       </a>
     </FooterWrapper>
   );
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,6 @@
 export const BASE_URL = "https://en.wikipedia.org//w/api.php";
 export const NUM_ENTRIES_TO_FETCH = 20;
-export const RANDOM_ARTICLE_URL = "https://en.wikipedia.org/wiki/Special:Random";
+export const RANDOM_ARTICLE_URL =
+  "https://en.wikipedia.org/wiki/Special:Random";
+export const GITHUB_URL =
+  "https://www.github.com/orrsteinberg/wikipedia-viewer";
