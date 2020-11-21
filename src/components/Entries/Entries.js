460a1173c42bda8a8e6ce817d7960abbcc74462d
Orr Steinberg <orr.sun10@gmail.com>
2020-11-21 15:27:47 +0200

    add mobile device check to render mobile wiki links on appropriate devices

--- src/components/Entries/Entries.js
+++ src/components/Entries/Entries.js
@@ -1,14 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Entry from "./Entry";
+
+import { isMobile } from "../../utils";
 import { EntriesContainer, LoadMore, LoadMoreButton } from "./Entries.elements";
+import Entry from "./Entry";
 
 const Entries = ({ entries, searchMore }) => {
+  // Check if the user is on a mobile device to create appropriate entry links
+  const mobileDevice = isMobile();
+
   return (
     <main>
       <EntriesContainer>
         {Object.values(entries).map((entry) => (
-          <Entry key={entry.pageid} entry={entry} />
+          <Entry key={entry.pageid} entry={entry} mobileDevice={mobileDevice} />
         ))}
         <LoadMore>
           <LoadMoreButton onClick={() => searchMore()}>
