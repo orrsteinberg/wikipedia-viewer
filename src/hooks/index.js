8393d5c4baf5437e42bef0f9ae780cf1c0969c69
Orr Steinberg <orr.sun10@gmail.com>
2020-12-01 21:15:42 +0200

    add useSerachHistory export

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,2 +1,3 @@
 export { default as useWikiSearch } from "./useWikiSearch";
 export { default as useBookmarks } from "./useBookmarks";
+export { default as useSearchHistory } from "./useSearchHistory";
